83d0fc2e78e10a1e7e0afb0f21c3f21804092327
Julien Malka <julien.malka@me.com>
2018-03-17 09:59:10 +0100

    Added block verification

--- BlockChain.py
+++ BlockChain.py
@@ -1,8 +1,21 @@
+from Block import *
 class BlockChain:
     def __init__(self):
-        self.chain = list
+        self.chain = list[Block]
 
 
+
+
+    def is_valid(self):
+        for i in range(1, len(self.chain)):
+            last_block = self.chain(i-1)
+            current_block = self.chain(i)
+            if current_block.hash != current_block.compute_hash:
+                return False
+            if last_block.hash != current_block.last_hash:
+                return False
+
+        return True
 
 
 
